2e5e7e83b36b22aaf7186e24cb9ef067494b8cbd
artemiiberko <artemii.berko@gmail.com>
2022-01-28 15:34:37 +0100

    10th commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,9 @@
 // material
 import { styled } from '@mui/material/styles';
-import { Card, Stack, Container, Typography } from '@mui/material';
+import { Stack, Container, Typography } from '@mui/material';
 // layouts
 // components
 import Page from '../components/Page';
-import { MHidden } from '../components/@material-extend';
 import { LoginForm } from '../components/authentication/login';
 
 // ----------------------------------------------------------------------
@@ -13,15 +12,6 @@ const RootStyle = styled(Page)(({ theme }) => ({
   [theme.breakpoints.up('md')]: {
     display: 'flex'
   }
-}));
-
-const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
-  maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  margin: theme.spacing(2, 0, 2, 2)
 }));
 
 const ContentStyle = styled('div')(({ theme }) => ({
@@ -39,15 +29,6 @@ const ContentStyle = styled('div')(({ theme }) => ({
 export default function Login() {
   return (
     <RootStyle title="Login | Minimal-UI">
-      <MHidden width="mdDown">
-        <SectionStyle>
-          <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-            Hi, Welcome Back
-          </Typography>
-          <img src="/static/illustrations/illustration_login.png" alt="login" />
-        </SectionStyle>
-      </MHidden>
-
       <Container maxWidth="sm">
         <ContentStyle>
           <Stack sx={{ mb: 5 }}>
